3284d772759c87e32d544d633d4cfb88fa309870
Kuldeep Upreti <kuldeep.upreti@coloredcow.com>
2023-08-21 12:18:18 +0530

    wip: remove db.model

--- app.py
+++ app.py
@@ -39,14 +39,11 @@ from wtforms.validators import DataRequired, EqualTo
 import pandas as pd
 from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
-from sqlalchemy.ext.declarative import DeclarativeMeta
 
 load_dotenv()
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
-
-BaseModel: DeclarativeMeta = db.Model
 
 # Change 'your_secret_key' to a strong and unique secret key
 app.secret_key = "randomsecret"
@@ -86,7 +83,7 @@ s3 = boto3.client(
 )
 
 
-class User(UserMixin, BaseModel):
+class User(UserMixin):
     """
     Represents a user in the application.
 
