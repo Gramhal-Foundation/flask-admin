427a60b0de4158cbd3a2de532bdaa4f80a0834c9
Vaibhav Rathore <vaibhav@coloredcow.com>
2023-09-13 18:39:25 +0530

    dependencies

--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
 from setuptools import setup, find_packages
 
+extras_require = {
+    'aws': ['boto']
+}
+
+install_requires = [
+    'Flask>==2.3',
+    'alembic',
+    'inflect',
+    'boto3',
+    'pandas',
+    'Flask-Login',
+    'Flask-WTF',
+    'Werkzeug',
+    'WTForms',
+    'SQLAlchemy'
+]
+
 setup(
     name='flask-admin',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=install_requires,
+    extras_require=extras_require
 )
