af0d3a34c722f15abf290fb617f085293b1bcaeb
Vaibhav Rathore <vaibhav@coloredcow.com>
2023-11-25 15:09:35 +0530

    precommit action hook

--- setup.py
+++ setup.py
@@ -1,27 +1,25 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-extras_require = {
-    'aws': ['boto']
-}
+extras_require = {"aws": ["boto"]}
 
 install_requires = [
-    'Flask>=0.7',
-    'alembic',
-    'inflect',
-    'boto3',
-    'pandas',
-    'Flask-Login',
-    'Flask-WTF',
-    'Werkzeug',
-    'WTForms',
-    'SQLAlchemy'
+    "Flask>=0.7",
+    "alembic",
+    "inflect",
+    "boto3",
+    "pandas",
+    "Flask-Login",
+    "Flask-WTF",
+    "Werkzeug",
+    "WTForms",
+    "SQLAlchemy",
 ]
 
 setup(
-    name='flask-admin',
-    version='0.1',
+    name="flask-admin",
+    version="0.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=install_requires,
-    extras_require=extras_require
+    extras_require=extras_require,
 )
